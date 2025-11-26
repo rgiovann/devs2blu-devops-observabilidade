eb4f5970738ed6e7a8d3b10065818c2435b97b3c
Giovani L. Rozza <giovanni.rozza@gmail.com>
2025-11-26 20:23:40 -0300

    gerador de carga aumentou as escritas no banco

--- load-generator/load.sh
+++ load-generator/load.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 echo "Waiting for database..."
-until mysql -h db -uappuser -papppass appdb -e "SELECT 1" &>/dev/null; do
+until mysql -h db -uappuser -papppass appdb -e "SELECT 1" >/dev/null 2>&1; do
   sleep 2
 done
-
 echo "Starting load generator..."
 while true; do
-  mysql -h db -uappuser -papppass appdb \
-    -e "INSERT INTO healthcheck VALUES (NULL, NOW());" 2>/dev/null || echo "Insert failed, retrying..."
-  sleep 5
+  mysql -h db -uappuser -papppass appdb <<EOF
+INSERT INTO healthcheck VALUES
+  (NULL, NOW()),
+  (NULL, NOW()),
+  (NULL, NOW()),
+  (NULL, NOW()),
+  (NULL, NOW());
+EOF
+  sleep 0.3
 done
